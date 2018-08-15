e2dcacd2080c7e92a7dab8087997736314c418b3
jmleglise <jmleglise@gmail.com>
2018-08-15 19:29:32 +0200

    Update script_time_FireAlarm.lua
    Correct some bugs

--- LuaScript/script_time_FireAlarm.lua
+++ LuaScript/script_time_FireAlarm.lua
@@ -1,25 +1,27 @@
 --[[    ~/domoticz/scripts/lua/script_time_FireAlarm.lua
-auteur : papoo
-version : 1.10
-MAJ : 16/08/2016
+Original Author : papoo / URL post : http://easydomoticz.com/forum/viewtopic.php?f=17&t=2319
 version 1.1 : ajout fonctionnalité envoi par mail uniquement
-création : 15/08/2016
-Principe : ce script vérifie toutes les deux minutes si il n'y a pas une augmentation de température anormale dans une des pièces référencées dans le tableau les_températures. le nom de chaque sonde doit être encadrer de " et suivi d'une virgule.  exemple  :"nom sonde 1", "nom sonde2",
-il compare chaque température au seuil fixé par la variable  seuil_notification (en °). Si une ou plusieurs températures sont supérieures à ce seuil, envoie d'une notification pour chacune d'elle.
-/!\ si le seuil est fixé trop bas, cela risque de générer beaucoup de notifications et d'éventuellement bloquer les services de type pushbullet.
-URL post : http://easydomoticz.com/forum/viewtopic.php?f=17&t=2319
+
+Check Every Minute the temperature of all temperature sensor and Send Alert if at least one of them
+exceed the predefined threshold.
+
+The alert is send by :
+- RealTime Voice TextToSpeak
+- notification
+- email
+
 ]]--
 
 package.path = package.path .. ';' .. '/home/pi/domoticz/scripts/lua/?.lua'
+My = require('My_Library')
 require('My_Config')
-
 --------------------------------------------
 ------------ Variables à éditer ------------
 --------------------------------------------
 
 local debugging = true     -- true pour voir les logs dans la console log Dz ou false pour ne pas les voir
-local seuil_notification = 42       -- seuil température au delà duquel les notifications d'alarme seront envoyées
-local les_temperatures = { "Sonde Veranda", "Sonde Parent", "Sonde Justine", "Thermostat", "Sonde THGN500", "Sonde Marion" };
+local seuil_temp_alarm = 42       -- seuil température au delà duquel les notifications d'alarme seront envoyées
+local les_temperatures = { "Sonde Veranda", "Sonde Parent", "Sonde Justine", "Thermostat", "Sonde Marion" };
 local notif_push = 1
 local notif_email = 1
 local notif_speak = 1
@@ -41,15 +43,16 @@ end
 --------------------------------------------
 -------------- Fin Fonctions ---------------
 --------------------------------------------
-local message = ""
+local messageVocal = ""
+local messageText = ""
 local alarmes = 0
 
 commandArray = {}
 
 now = os.date("*t")
 
-if now.min % 2 == 0 then
-    voir_les_logs("--- --- --- script_time_AlarmeIncendie.lua --- Seuil à " .. seuil_notification .. "°C", debugging)
+if now.min % 1 == 0 then
+    voir_les_logs("--- --- --- script_time_FireAlarm.lua --- Test toutes les sondes. Seuil à " .. seuil_temp_alarm .. "°C", debugging)
 
     for i, d in ipairs(les_temperatures) do
         local v = otherdevices[d]
@@ -61,10 +64,10 @@ if now.min % 2 == 0 then
                 --              voir_les_logs("--- --- --- svalue " .. d .. " = " .. (v or "nil"), debugging)
             end
 
-            if tonumber(v) > tonumber(seuil_notification) then
+            if tonumber(v) > tonumber(seuil_temp_alarm) then
                 alarmes = alarmes + 1
-                messageVocal = messageVocal .. d .. v
-                messageText = messageText .. "sonde :" .. d .. "Temp:" .. v
+                messageVocal = messageVocal .. d .. " "
+                messageText = messageText .. "<br>Sonde :" .. d .. " Temp:" .. v
             end
         end
         --        else
@@ -74,20 +77,19 @@ if now.min % 2 == 0 then
 
     if alarmes > 1 then
         if notif_speak == 1 then
-            messageVocal = "Alerte incendie. La température dépasse le seuil d'alerte sur" .. messageVocal
+            messageVocal = "Alerte incendie. Seuil de température dépassé sur " .. messageVocal
             My.Speak(messageVocal, "fort")
         end
 
         if alarmes > 1 and notif_email == 1 then
-            commandArray['SendEmail'] = 'Alerte incendie#'..os.date("%H:%M")..' -- La température dépasse le seuil fixé à ' .. seuil_notification .. '°C<br>' .. messageText .. '#' .. EmailTo
+            commandArray['SendEmail'] = 'Alerte incendie#'..os.date("%H:%M")..' -- La température dépasse le seuil fixé à ' .. seuil_temp_alarm .. '°C<br>' .. messageText .. '#' .. EmailTo
         end
 
         if alarmes > 1 and notif_push == 1 then
-            commandArray[#commandArray + 1] = { ['SendNotification'] = 'Alerte incendie#'..os.date("%H:%M").. ' -- La température dépasse le seuil fixé à ' .. seuil_notification .. '°C<br>' .. messageText }
+            commandArray[#commandArray + 1] = { ['SendNotification'] = 'Alerte incendie#'..os.date("%H:%M").. ' -- La température dépasse le seuil fixé à ' .. seuil_temp_alarm .. '°C<br>' .. messageText }
         end
 
-        voir_les_logs("--- --- --- " .. objet, debugging)
-        voir_les_logs("--- --- --- " .. message, debugging)
+        voir_les_logs("--- --- --- " .. messageVocal, debugging)
     end
 
 end -- if now
