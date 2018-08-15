30e4069b749cf50de8323ed549e71d05b5293dc6
jmleglise <jmleglise@gmail.com>
2018-08-15 19:53:16 +0200

    Update script_device_Alarm_NightnDays.lua
    Some improvements and documentation

--- LuaScript/script_device_Alarm_NightnDays.lua
+++ LuaScript/script_device_Alarm_NightnDays.lua
@@ -1,16 +1,39 @@
---[[
-~/domoticz/scripts/lua/script_device_Alarm_NightnDays.lua
+--[[  ~/domoticz/scripts/lua/script_device_Alarm_NightnDays.lua
+Author : jmleglise
+Version : 1.1
+
+Real nice Alarm system with :
+- Voice rendering to disturbed burglars
+- audio siren by mp3
+- Hardware Siren external and internal. ( i am using a KMtronic relay card by USB)
+- Physical switch to disarm the alarm
+
+"Night" Mode :
+If sensors near the Sleeping Room detect motion before other sensors, then the alarm is disarmed. That Means that a familly member get up at night.
+In fact , I have a house on several leval. (sleeping rooms are upstairs.)
+
+A detection, only triggered voice and siren.mp3 to wake up the house.
+Si mouvement entre 0h30 et 6h30 et pas de mouvement upstairs dans les 3 minutes. A chaque détection au salon (repetition)
+Si mouvement upstairs : alarme désactivée
+
+"On" Mode :
+A detection triggered voice and physical siren external and internal the house.
+
+
 
 REQUIRE :
-devices['Alarm Mode']
+devices['Alarm Mode']    with 4 modes  :    Off Night On Auto
 devices['Alarm Sound Timer']
-devices['Alarm Switch']
+devices['Alarm Switch']     Interrupteur qui désactive l'alarme
 devices['Relay 8 - External Siren']
 devices['Relay 7 - Internal Siren']
-'Motion LivingRoom'
-'Motion Veranda'
+'Motion LivingRoom'   a wired Motion sensor, directly connect to the gpio of the Pi
+'Motion Veranda'   433Mhz
+'Motion Upstairs'   433Mhz
 
--- conditions : tous les détecteurs doivent se remettre automatiquement au repos en moins de 5 minutes
+Require : All Motion sensor must automatically turn Off after a detection in less than 5 minutes.
+Pas de detection dans les 5 minutes après un changement de "Alarm Mode".
+
 
 ]]--
 
@@ -20,13 +43,13 @@ My = require('My_Library')
 
 local time = os.date("*t")
 local minutes = time.min + time.hour * 60
-local idxAlarmSoundTimer = '113'                    -- Your Alarm Sound Timer Switch Device IDX
+local idxAlarmSoundTimer = '113'                    -- The Alarm Sound Timer Switch Device IDX
 
 commandArray = {}
 
 device = tostring(next(devicechanged))
 
--- ################# Alarm armed. Warning message
+-- ################# User Set Alarm to On. Warning message
 if device == "Alarm Mode" and otherdevices[device] == 'On' then
     My.Speak("Alarme activée. Vous avez 4 minutes pour sortir de la maison !", "normal")
     return commandArray
@@ -70,7 +93,7 @@ if device == "Alarm Sound Timer" and otherdevices[device] == 'On' and otherdevic
     return commandArray
 end
 
--- ################# Detect Intrusion
+-- ################# Detect Intrusion    pour jour et nuit
 
 if (device == 'Motion LivingRoom' or device == 'Motion Veranda') and otherdevices[device] == 'On' -- It s a motion and ON
 and My.Time_Difference(otherdevices_lastupdate['Alarm Mode']) > 5 * 60    -- le mode Alarme n'a pas été changé dans les 5 dernières minutes (cela permet de quitter la maison, et que tous les détecteurs repassent au repos) : timer to leave the house when alarm is activated
@@ -91,7 +114,7 @@ then
         -- détection motion toutes les 100 secondes (autoinit du Motion)
         -- arme Timer 10 secondes
 
-        commandArray[#commandArray + 1] = { ['Alarm Sound Timer'] = 'On AFTER 10' }   -- Arme le Timer. Sirene in 10 secondes 			
+        commandArray[#commandArray + 1] = { ['Alarm Sound Timer'] = 'On AFTER 10' }   -- Arme le Timer. Sirene in 10 secondes
 
         if My.Time_Difference(otherdevices_lastupdate['Alarm Sound Timer']) > 20 * 60   --ne sert que pour ne pas répéter la même phrase. Car un test est fait sur la répétition de la sirene
         then
@@ -100,7 +123,7 @@ then
             -- TODO prendre photo !!!
             My.Speak("Qui est là ? Jappelle la police. Alarme dans 5 secondes", "fort")   -- 10secondes en vrai
         else -- il y a déjà eu un déclenchement en moins de 15 minutes
-            My.Speak("La police arrive. Allez-vous en.", "fort")
+            My.Speak("La police est prévenue. Allez-vous en.", "fort")
             commandArray[#commandArray + 1] = { ['SendNotification'] = 'Maison/Alarm - Intrusion detected again. Arm the 10 seconds timer. # Speak Go away.' .. time.hour .. 'h' .. time.min .. '#0' }
             -- TODO prendre photo !!!
         end
@@ -108,7 +131,7 @@ then
 
     -- #####################  Security by night  between 0h30 and 6h00
     if otherdevices['Alarm Mode'] == 'Night' -- check for security by night
-    and minutes > 30 and minutes < 6 * 60
+    and minutes > 30 and minutes < (6 * 60+30)    -- between 0h30 et 6h30
     then
         if My.Time_Difference(otherdevices_lastupdate['Motion Upstairs']) > 3 * 60    -- 3 minutes
         then
@@ -116,10 +139,10 @@ then
             My.Speak("Qui est là ? Jappelle la police.", "fort", "siren")
             commandArray[#commandArray + 1] = { ['SendNotification'] = 'Maison/Alarm - Intrusion detected by night. # Speak Who s here ? ' .. time.hour .. 'h' .. time.min .. '#0' }
             -- TODO: prendre photo !!!
-        else -- Détection + membre de la famille vient de l'étage. => désactive l'alarme pour le reste de la nuit.
+        else -- Motion detected downstairs + there was previously a motion upstairs = A family member is awake. it's not an intrusion. => disarm the alarm for the rest of the night.
             My.Speak("Alarme nocturne désactivée pour le reste de la nuit.", "faible")
             commandArray[#commandArray + 1] = { ['Alarm Mode'] = 'Off' }
-            commandArray[#commandArray + 1] = { ['Alarm Mode'] = 'On AFTER 36000' } -- Réactive l'alarme 10h après. (au plus tard c'est à 16h)
+--            commandArray[#commandArray + 1] = { ['Alarm Mode'] = 'On AFTER 36000' } -- Réactive l'alarme 10h après. (au plus tard c'est à 16h)
             commandArray[#commandArray + 1] = { ['SendNotification'] = 'Maison/Alarm - Authorized Move by night. Disarm Alarm until tomorrow# ' .. time.hour .. 'h' .. time.min .. '#0' }
         end
     end
