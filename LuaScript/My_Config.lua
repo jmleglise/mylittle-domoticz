d5e3e606545edf66aa733c3c38374d201c616e62
jmleglise <jmleglise@gmail.com>
2019-06-01 23:35:01 +0200

    add lat & long
    someone knows how to get them from domoticz settings ?

--- LuaScript/My_Config.lua
+++ LuaScript/My_Config.lua
@@ -2,3 +2,6 @@ MY_CONFIG_EMAIL="xxxxx@gmail.com"
 WU_API_KEY="e27xxxxx4c"
 DS_API_KEY="dd63xxxxxxx097"    -- DarkSky
 OW_API_KEY="57dxxxxxx322d"      -- openweather
+
+MY_LATITUDE='48.000000'
+MY_LONGITUDE='2.000000'
