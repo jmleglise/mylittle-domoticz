3e227170e1344eac365ab5f8b961281d53944606
jmleglise <jmleglise@gmail.com>
2019-06-01 23:14:12 +0200

    Update My_Config.lua

--- LuaScript/My_Config.lua
+++ LuaScript/My_Config.lua
@@ -1,2 +1,4 @@
-MY_CONFIG_EMAIL="xxx@gmail.com"
-WU_API_KEY="eeeeeeeeeeeeeee"
+MY_CONFIG_EMAIL="xxxxx@gmail.com"
+WU_API_KEY="e27xxxxx4c"
+DS_API_KEY="dd63xxxxxxx097"    -- DarkSky
+OW_API_KEY="57dxxxxxx322d"      -- openweather
