fa4bc98adb39aea75a8613e30793f481672d3035
jmleglise <jmleglise@gmail.com>
2020-01-22 14:50:34 +0100

    5 Retries without response of the Meteo France API.

--- LuaScript/script_time_PrevisionPluie.lua
+++ LuaScript/script_time_PrevisionPluie.lua
@@ -58,63 +58,79 @@ local EMAIL = MY_CONFIG_EMAIL  -- adresse mail, séparées par ; si plusieurs
 ---------------------------------------------------------------------------
 commandArray = {}
 now = os.date("*t")
-if now.min % 5 == 0 then
+if now.min % 10 == 0 then
     print('--- --- --- script_time_meteofrance_pluie.lua ---')
     json = (loadfile "/home/pi/domoticz/scripts/lua/JSON.lua")()
 
-    local config = assert(io.popen('curl http://www.meteofrance.com/mf3-rpc-portlet/rest/pluie/' .. CITY_CODE .. '.json'))
-    local fileContent = config:read('*all')
-    config:close()
-    local jsonPrevision = json:decode(fileContent)
+    fileContent=""
+    for i = 1, 5 do
+        if string.len(fileContent) <100 then
+            local config = assert(io.popen('curl http://www.meteofrance.com/mf3-rpc-portlet/rest/pluie/' .. CITY_CODE))
+            fileContent = config:read('*all')
+            config:close()
+        end
+    end    
 
-    rainLevel = 0
-    when = 0
-    data = jsonPrevision.dataCadran
-    for i = 1, 12 do
-        if data[i].niveauPluie > 1 and when == 0 then
-            -- Find the first time that the rain is announced
-            when = i
-        end
-        if data[i].niveauPluie > rainLevel then
-            -- Find the maximum level of rain announced
-            rainLevel = data[i].niveauPluie
-        end
-    end
-
-    if rainLevel == 0 then
-        commandArray[#commandArray + 1] = { ['SendEmail'] = 'Domoticz Anomalie#' .. os.date("%H:%M") .. ' -- Anomalie dans le script : script_time_PrevisionPluie.lua<br> L\'Api de meteoFrance renvoie un JSON inattendu. niveauPluie n\'est pas correct (devrait être de 1 à 4)<br>Contenu du Json : -------------<br>' .. fileContent .. '#' .. EMAIL }
-    elseif rainLevel == 1 then
-        if otherdevices_svalues["Alerte Pluie dans l\'heure"] ~= "Pas de pluie dans l\'heure" then
-            -- Don't update the device if there is no change. (so that don't change the timestamp of the devie)
-            commandArray[#commandArray + 1] = { ['UpdateDevice'] = RAIN_ALERT_IDX .. "|1|Pas de pluie dans l\'heure" }
-        end
+    if string.len(fileContent) <100 then
+        print("API Error:"..string.len(fileContent))
     else
-        if when == 1 then
-            whenText = " maintenant"
+        local jsonPrevision = json:decode(fileContent)
+    
+        rainLevel = 0
+        when = 0
+        data = jsonPrevision.dataCadran
+        for i = 1, 12 do
+            if data[i].niveauPluie > 1 and when == 0 then
+                -- Find the first time that the rain is announced
+                when = i
+            end
+            if data[i].niveauPluie > rainLevel then
+                -- Find the maximum level of rain announced
+                rainLevel = data[i].niveauPluie
+            end
+        end
+    
+        if when==0 then 
+            print('Pas de pluie annoncée')
+        end
+        if rainLevel == 0 then -- données indisponibles
+           -- commandArray[#commandArray + 1] = { ['SendEmail'] = 'Domoticz Anomalie#' .. os.date("%H:%M") .. ' -- Anomalie dans le script : script_time_PrevisionPluie.lua<br> L\'Api de meteoFrance renvoie un JSON inattendu. niveauPluie n\'est pas correct (devrait être de 1 à 4)<br>Contenu du Json : -------------<br>' .. fileContent .. '#' .. EMAIL }
+        elseif rainLevel == 1 then
+            if otherdevices_svalues["Alerte Pluie dans l\'heure"] ~= "Pas de pluie dans l\'heure" then
+                -- Don't update the device if there is no change. (so that don't change the timestamp of the devie)
+                commandArray[#commandArray + 1] = { ['UpdateDevice'] = RAIN_ALERT_IDX .. "|1|Pas de pluie dans l\'heure" }
+            end
         else
-            whenText = " dans " .. tostring(when * 5) .. " minutes"
-        end
-
-        if rainLevel == 2 then
-            rainLevelText = "légère"
-        elseif rainLevel == 3 then
-            rainLevelText = "modérée"
-        elseif rainLevel == 4 then
-            rainLevelText = "forte"
-        end
-
-        commandArray[#commandArray + 1] = { ['UpdateDevice'] = RAIN_ALERT_IDX .. '|' .. rainLevel .. '|Pluie ' .. rainLevelText .. whenText }
-        if My.Time_Difference(otherdevices_lastupdate["Alerte Pluie dans l\'heure"]) > 3 * 60 * 60 then
-            if NOTIFICATION_VOCAL == true and now.hour > 8 and now.hour <= 20 then
-                My.Speak("Je vous dérange pour vous prévenir qu\'une pluie " .. rainLevelText .. " est annoncée par méteo france" .. whenText, "normal")
+            if when == 1 then
+                whenText = " maintenant"
+            else
+                whenText = " dans " .. tostring(when * 5) .. " minutes"
             end
-            if NOTIFICATION_PUSH == true then
-                commandArray[#commandArray + 1] = { ['SendNotification'] = 'Alerte Pluie#Pluie ' .. rainLevelText .. whenText }
+    
+            if rainLevel == 2 then
+                rainLevelText = "légère"
+            elseif rainLevel == 3 then
+                rainLevelText = "modérée"
+            elseif rainLevel == 4 then
+                rainLevelText = "forte"
+            else
+                commandArray[#commandArray + 1] = { ['SendEmail'] = 'Domoticz Anomalie#' .. os.date("%H:%M") .. ' -- Anomalie dans le script : script_time_PrevisionPluie.lua<br> L\'Api de meteoFrance renvoie un JSON inattendu. niveauPluie est en cas non traité (devrait être de 0 , 1 à 4)<br>Contenu du Json : -------------<br>' .. fileContent .. '#' .. EMAIL }
             end
-            if (DEBUG_MODE) then
-                commandArray[#commandArray + 1] = { ['SendEmail'] = 'JSON Alerte Pluie#' .. os.date("%H:%M") .. ' -- Api de meteoFrance <br>Contenu du Json : -------------<br>' .. fileContent .. '#' .. EMAIL }
+    
+            commandArray[#commandArray + 1] = { ['UpdateDevice'] = RAIN_ALERT_IDX .. '|' .. rainLevel .. '|Pluie ' .. rainLevelText .. whenText }
+            if My.Time_Difference(otherdevices_lastupdate["Alerte Pluie dans l\'heure"]) > 3 * 60 * 60 then
+                if NOTIFICATION_VOCAL == true and now.hour > 8 and now.hour <= 20 then
+                    My.Speak("alexa","Je vous dérange un instant. Une pluie " .. rainLevelText .. " est annoncée " .. whenText, "normal")
+                end
+                if NOTIFICATION_PUSH == true then
+                    commandArray[#commandArray + 1] = { ['SendNotification'] = 'Alerte Pluie#Pluie ' .. rainLevelText .. whenText }
+                end
+                if (DEBUG_MODE) then
+                    commandArray[#commandArray + 1] = { ['SendEmail'] = 'JSON Alerte Pluie#' .. os.date("%H:%M") .. ' -- Api de meteoFrance <br>Contenu du Json : -------------<br>' .. fileContent .. '#' .. EMAIL }
+                end
             end
         end
-    end
+    end 
 end --if now.min
 return commandArray
+
